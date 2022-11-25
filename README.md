079f71ebe400cf158aee2400393a424682bd1a63
Ze-Zheng Wu <zzwu@zju.edu.cn>
2022-11-25 18:00:26 +0800

    feat: add fromIterable

--- README.md
+++ README.md
@@ -12,10 +12,32 @@ npm i @sec-ant/readable-stream
 
 ## Usage
 
-This package is used as a side effect.
+This package can be used as a side effect if you only need async iteration support:
 
 ```ts
 import "@sec-ant/readable-stream";
+```
+
+You can also import the `fromIterable` function which enables you to create a `ReadableStream` from an `Iterable` or an `AsyncIterable`:
+
+```ts
+import { fromIterable } from "@sec-ant/readable-stream";
+```
+
+With the help of this package, you can `tee` an `AsyncIterable` like this:
+
+```ts
+import { fromIterable } from "@sec-ant/readable-stream";
+
+async function* createAsyncIterable() {
+  yield "how";
+  yield "are";
+  yield "you";
+}
+
+const asyncIterable = createAsyncIterable();
+
+const [asyncIterable1, asyncIterable2] = fromIterable(asyncIterable).tee();
 ```
 
 ## License
--- src/index.ts
+++ src/index.ts
@@ -45,3 +45,46 @@ ReadableStream.prototype.values ??= ReadableStream.prototype[
 
 ReadableStream.prototype[Symbol.asyncIterator] ??=
   ReadableStream.prototype.values;
+
+export function fromIterable<R = unknown>(
+  iterable: Iterable<R> | AsyncIterable<R>
+) {
+  if (iterable instanceof ReadableStream) {
+    return iterable;
+  }
+  let iterator: AsyncIterator<R, undefined> | Iterator<R, undefined>;
+  return new ReadableStream<R>({
+    start() {
+      if (isIterable(iterable)) {
+        iterator = iterable[Symbol.iterator]();
+      } else if (isAsyncIterable(iterable)) {
+        iterator = iterable[Symbol.asyncIterator]();
+      } else {
+        throw new Error("Not an iterable: " + iterable);
+      }
+    },
+    async pull(controller: ReadableStreamDefaultController<R>) {
+      const { value, done } = await iterator.next();
+      if (done) {
+        controller.close();
+        return;
+      }
+      controller.enqueue(value);
+    },
+    cancel(reason) {
+      iterator.return && iterator.return(reason);
+    },
+  });
+}
+
+function isIterable<R = unknown>(
+  iterable: Iterable<R> | AsyncIterable<R>
+): iterable is Iterable<R> {
+  return Symbol.iterator in iterable;
+}
+
+function isAsyncIterable<R = unknown>(
+  iterable: Iterable<R> | AsyncIterable<R>
+): iterable is AsyncIterable<R> {
+  return Symbol.asyncIterator in iterable;
+}
